4240ff99c773c3e1b5224ac67eb61cc0d14ab888
Dan <dandonnan@outlook.com>
2023-12-05 22:50:17 +0000

    - Removed Inputs as they aren't supposed to be shared
    - Rejigged tests

---
+++ AdventTests/Puzzles/PuzzleTestModel.cs
@@ -0,0 +1,27 @@
+﻿using Advent.Puzzles;
+
+namespace AdventTests.Puzzles
+{
+    internal class PuzzleTestModel
+    {
+        // The puzzle to test
+        public IPuzzle Puzzle { get; set; }
+
+        // The name of the input file
+        public string InputFile { get; set; }
+
+        // The expected result for puzzle 1
+        public string Puzzle1ExpectedResult { get; set; }
+
+        // The expected result for puzzle 2
+        public string Puzzle2ExpectedResult { get; set; }
+
+        // Whether the input files are different for each puzzle
+        public bool SplitInputs { get; set; }
+
+        public override string ToString()
+        {
+            return InputFile;
+        }
+    }
+}
---
+++ AdventTests/Puzzles/PuzzleTestRunner.cs
@@ -0,0 +1,76 @@
+﻿using Advent;
+using NUnit.Framework.Internal;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AdventTests.Puzzles
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    internal class PuzzleTestRunner
+    {
+        [Test]
+        [TestCaseSource(nameof(GetTests))]
+        public void SolvePuzzle1(PuzzleTestModel test)
+        {
+            string testFile = test.InputFile;
+
+            // If the input file is split for each puzzle, use the correct one
+            if (test.SplitInputs)
+            {
+                testFile = $"{testFile}1";
+            }
+
+            // Test that the solution to the first puzzle matches the expected result
+            Assert.That(test.Puzzle1ExpectedResult, Is.EqualTo(test.Puzzle.SolvePuzzle1(testFile)));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetTests))]
+        public void SolvePuzzle2(PuzzleTestModel test)
+        {
+            string testFile = test.InputFile;
+
+            // If the input file is split for each puzzle, use the correct one
+            if (test.SplitInputs)
+            {
+                testFile = $"{testFile}2";
+            }
+
+            // Test that the solution to the second puzzle matches the expected result
+            Assert.That(test.Puzzle2ExpectedResult, Is.EqualTo(test.Puzzle.SolvePuzzle2(testFile)));
+        }
+
+        // A list of tests to run with their expected inputs
+        private static List<PuzzleTestModel> GetTests()
+        {
+            return new List<PuzzleTestModel>
+            {
+                // Samples
+                CreateTestModel(1, "142", "281", true, true),
+                CreateTestModel(2, "8", "2286", true),
+                CreateTestModel(3, "4361", "467835", true),
+                CreateTestModel(4, "13", "30", true),
+
+                // Puzzles
+                CreateTestModel(1, "54632", "54019"),
+                CreateTestModel(2, "2685", "83707"),
+                CreateTestModel(3, "527446", "73201705"),
+                CreateTestModel(4, "21138", "7185540")
+            };
+        }
+
+        private static PuzzleTestModel CreateTestModel(int day, string expectedResult1, string expectedResult2, bool sample = false, bool splitInputs = false)
+        {
+            var puzzle = PuzzleFactory.GetPuzzle(day, out string file);
+
+            return new PuzzleTestModel
+            {
+                Puzzle = puzzle,
+                InputFile = sample ? $"{file}_sample" : file,
+                Puzzle1ExpectedResult = expectedResult1,
+                Puzzle2ExpectedResult = expectedResult2,
+                SplitInputs = splitInputs
+            };
+        }
+    }
+}
